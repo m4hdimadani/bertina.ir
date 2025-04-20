b9174bc29d23554a47e12fe2bf51781a77f080c5
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-04-20 18:35:49 +0330

    updaiting

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
 
-  speed:900,
+  speed: 900,
   autoplay: {
     delay: 3000,
   },
@@ -19,4 +19,24 @@ const swiper = new Swiper(".swiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
   },
+});
+
+////////////////////////////////////////////
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    const content = toggle.nextElementSibling;
+    const icon = toggle.querySelector(".icon");
+
+    const isOpen = content.style.maxHeight && content.style.maxHeight !== "0px";
+
+    if (isOpen) {
+      content.style.maxHeight = "0px";
+      icon.textContent = "+";
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      icon.textContent = "−";
+    }
+  });
 });
